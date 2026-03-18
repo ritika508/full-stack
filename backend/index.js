b654a4394a60c434cf0fda61221fcebffbad9703
ritika508 <ritikagautamgautam@gmail.com>
2026-03-18 15:10:03 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './src/utlis/dbConnect.js';
+dotenv.config();
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(cors())
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("API is running...")
+});
+
+  
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.error(`Error connecting to the database: ${error.message}`);
+    process.exit(1);
+});
